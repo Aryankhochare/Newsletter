771dc9b1b693c7a41f860ab118ad5c2f4c814d2e
BooTheCreator <boosolini123@gmail.com>
2024-07-25 15:28:19 +0530

    Added Adapter

--- Frontend/news/src/app/api/auth/[...nextauth].js
+++ Frontend/news/src/app/api/auth/[...nextauth].js
@@ -1,14 +1,24 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { SupabaseAdapter } from "@auth/supabase-adapter"
-export const authOptions = {
-  // DUMMY PROVIDERS
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+// export const authOptions = {
+//   // DUMMY PROVIDERS
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     // IDK HOW TO ADD CUSTOM LOGINS RN
+//   ],
+  
+// }
+
+export const {handlers, auth, signIn, signOut} = NextAuth({
+    providers : [],
+    adapter : SupabaseAdapter({
+        url : process.env.SUPERBASE_URL,
+        secret : process.env.SUPERBASE_SERVICE_ROLE_KEY,
     }),
-    // IDK HOW TO ADD CUSTOM LOGINS RN
-  ],
-}
-export default NextAuth(authOptions)
+})
+
+//export default NextAuth(authOptions)
