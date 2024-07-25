877768fca5f2d9afc361bc7291f3f42157245eb2
Sayhan Ali <97283226+nocluee@users.noreply.github.com>
2024-07-25 12:05:22 +0530

    adding Backend

---
+++ Backend/Newsletter/Models/UserRoles.cs
@@ -0,0 +1,17 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+
+namespace Newsletter.Models
+{
+    [Table("UserRoles")]
+    public class UserRoles : BaseModel
+    {
+        [PrimaryKey("user_role_id",false)]
+        public long UserRoleId { get; set; }
+        [Column("user_role_name")]
+        public string UserRoleName { get; set; }
+
+    }
+}
