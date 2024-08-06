a1005b052fc75cb5ac80290a0521ad61037055f1
Sayhan Ali <97283226+nocluee@users.noreply.github.com>
2024-08-06 11:44:14 +0530

    implemented sign up

---
+++ Backend/Newsletter/ViewModels/GetAllUsersVM.cs
@@ -0,0 +1,25 @@
+﻿using Newsletter.Models;
+using Supabase.Postgrest.Attributes;
+
+namespace Newsletter.ViewModels
+{
+    public class GetAllUsersVM
+    {
+   
+        public string Id { get; set; }
+     
+        public string Username { get; set; }
+  
+        public string Email { get; set; }
+
+        public bool isActive { get; set; }
+     
+        public string Status { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public List<UserUserRoles> UserUserRoles { get; set; }
+    }
+}
