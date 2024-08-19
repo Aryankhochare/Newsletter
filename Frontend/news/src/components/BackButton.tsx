f23e1075ea85df9a599e6d8bbf415aaa6935248b
Kushi-09 <159606408+Kushi-09@users.noreply.github.com>
2024-08-19 11:48:37 +0530

    changes

---
+++ Frontend/news/src/components/BackButton.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { Button } from './ui/button'
+
+const BackButton = () => {
+    const router = useRouter()
+    return (
+      <Button 
+        onClick={() => router.back()} 
+        className="absolute top-4 left-4 p-2 rounded-full bg-gray-800 hover:bg-gray-300 transition-colors duration-200"
+      >
+        <ArrowLeft size={24} />
+      </Button>
+    )
+}
+
+export default BackButton
