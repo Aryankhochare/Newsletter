d0677111b889d4beeeedead7296b6b3a78ac1556
Aryan khochare <aryankhochare184@gmail.com>
2024-08-19 10:35:52 +0530

    changes in  auth UI

--- Frontend/news/src/app/(auth)/login/page.tsx
+++ Frontend/news/src/app/(auth)/login/page.tsx
@@ -1,12 +1,113 @@
+// 'use client'
+
+// import Link from "next/link";
+// import { useState } from "react";
+// import { signIn, SignInResponse } from "next-auth/react";
+// import { useRouter } from "next/navigation";
+// //import { Button } from '@/components/button';
+// import { FcGoogle } from 'react-icons/fc';
+// //import { Label } from '@/components/label';
+
+// const LoginPageComp = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+//   const router = useRouter();
+
+//   const onSubmit = async (e : any) => {
+//     e.preventDefault();
+//     setError("");
+
+  //   try {
+  //     const result : any = await signIn("credentials", {
+  //       username,
+  //       password,
+  //       redirect: false,
+  //     });
+
+  //     if (result.error) {
+  //       setError("Invalid username or password");
+  //     } else {
+  //       router.push("/");
+  //     }
+  //   } catch (error) {
+  //     console.error("Login error:", error);
+  //     setError("An error occurred during login");
+  //   }
+  // };
+
+//   return (
+
+//     <main className="bg-black text-white min-h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
+//       <div className="w-3/4 h-3/5 max-w-md min-h-screen  bg-gray-800 text-white rounded-lg shadow-lg overflow-hidden">
+//         <div className="p-8 space-y-2 ">
+//           <h1 className="text-2xl sm:text-3xl font-semibold text-center mb-6">LOGIN</h1>
+//           <p className="text-gray-400 text-center mb-8">Welcome </p>
+
+//           <div>
+//             <button className="flex items-center justify-center w-full gap-4 bg-white text-black hover:bg-gray-100 rounded-full py-2 px-4">
+//               <FcGoogle className="text-xl" />
+//               <span>Sign In with Google</span>
+//             </button>
+//             </div>
+            
+            
+//           <form onSubmit={onSubmit} className="space-y-6">
+
+//             <div className="space-y-2">
+//               <label htmlFor="username" className="block">UserName</label>
+//               <input 
+//                 className="w-full p-3 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+//                 type="text"
+//                 id="username"
+//                 placeholder="Username"
+//                 onChange={(e) => setUsername(e.target.value)}
+//                 value={username}
+//               />
+//             </div>
+
+//             <div className="space-y-2">
+//               <label htmlFor="password" className="block">Password</label>
+//               <input 
+//                 className="w-full p-3 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+//                 type="password"
+//                 id="password"
+//                 placeholder="Password"
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 value={password}
+//               />
+//             </div>
+//             {error && <p className="text-red-500">{error}</p>}
+
+//             <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 rounded-full py-3">
+//               Login
+//             </button>
+//           </form>
+
+          // <div className="mt-4 text-center">
+          //   <p className="text-gray-400">Don&apos;t have an account?</p>
+          //   <a href="/signup" className="text-blue-400 hover:underline">Sign up</a>
+          // </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default LoginPageComp ;
+
+
+
+
+
 'use client'
 
 import Link from "next/link";
 import { useState } from "react";
-import { signIn, SignInResponse } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-//import { Button } from '@/components/button';
 import { FcGoogle } from 'react-icons/fc';
-//import { Label } from '@/components/label';
+import { FaFacebookF } from 'react-icons/fa';
 
 const LoginPageComp = () => {
   const [username, setUsername] = useState("");
@@ -37,61 +138,77 @@ const LoginPageComp = () => {
   };
 
   return (
+    <main className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
+      <div className="sm:w-full md:w-4/5 max-w-6xl bg-white rounded-lg shadow-md overflow-hidden flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 p-8 flex flex-col justify-center">
+          <h1 className="text-4xl font-bold text-black text-center mb-4">The Global Buzz</h1>
+          <p className="text-gray-600 text-center text-sm mb-8">"Delivering the World's Hottest Buzz, Packed with Global Insight"</p>
 
-    <main className="bg-black text-white min-h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
-      <div className="w-full max-w-md bg-gray-800 text-white rounded-lg shadow-lg overflow-hidden">
-        <div className="p-8 space-y-2">
-          <h1 className="text-2xl sm:text-3xl font-semibold text-center mb-6">LOGIN</h1>
-          <p className="text-gray-400 text-center mb-8">Welcome </p>
+          <p className="text-xs text-gray-500 mb-4">By continuing you indicate that you agree to The Global Buzz's Terms of Service and Privacy Policy.</p>
 
-          <div>
-            <button className="flex items-center justify-center w-full gap-4 bg-white text-black hover:bg-gray-100 rounded-full py-2 px-4">
-              <FcGoogle className="text-xl" />
-              <span>Sign In with Google</span>
-            </button>
-            </div>
-            
-            
-          <form onSubmit={onSubmit} className="space-y-6">
+          <button className="w-full flex items-center justify-center gap-2 bg-white border border-gray-300 text-gray-700 rounded-md py-2 px-4 hover:bg-gray-50 mb-4">
+            <FcGoogle className="text-xl" />
+            <span>Continue with Google</span>
+          </button>
 
+          <button className="w-full flex items-center justify-center gap-2 bg-blue-600 text-white rounded-md py-2 px-4 hover:bg-blue-700 mb-4">
+            <FaFacebookF className="text-xl" />
+            <span>Continue with Facebook</span>
+          </button>
+
+          <div className="text-center text-sm text-gray-500">
+            <a href="#" className="text-gray-500 hover:underline hover:text-black text-sm">Continue as Guest</a>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2   bg-gray-50 flex flex-col justify-center">
+        <div className="text-black text-2xl font-semibold text-center p-2  mt-0  ">Login</div>
+          <form onSubmit={onSubmit} className="space-y-4 p-8">
             <div className="space-y-2">
-              <label htmlFor="username" className="block">UserName</label>
+              <div className="text-sm">Username/Email:</div>
               <input 
-                className="w-full p-3 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full p-2 border border-gray-300 rounded-md"
                 type="text"
-                id="username"
-                placeholder="Username"
+                placeholder="Your email"
                 onChange={(e) => setUsername(e.target.value)}
                 value={username}
               />
             </div>
 
             <div className="space-y-2">
-              <label htmlFor="password" className="block">Password</label>
+            <div className="text-sm">Password:</div>
               <input 
-                className="w-full p-3 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full p-2 border border-gray-300 rounded-md"
                 type="password"
-                id="password"
-                placeholder="Password"
+                placeholder="Your password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
             </div>
-            {error && <p className="text-red-500">{error}</p>}
 
-            <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 rounded-full py-3">
-              Login
-            </button>
-          </form>
+            {error && <p className="text-red-500 text-sm">{error}</p>}
 
-          <div className="mt-4 text-center">
+            <div className="flex justify-between items-center">
+              <a href="#" className="text-sm text-gray-500 hover:underline hover:text-black">Forgot password?</a>
+              <button type="submit" className="bg-black text-white rounded-md py-1 px-4 hover:bg-slate-800  ">
+                Login
+              </button>
+
+            </div>
+            <div className="mt-4 text-center">
             <p className="text-gray-400">Don&apos;t have an account?</p>
-            <a href="/signup" className="text-blue-400 hover:underline">Sign up</a>
+            <a href="/signup" className="text-gray-500 hover:underline hover:text-black">Sign up</a>
           </div>
+          </form>
         </div>
       </div>
+
+      {/* <div className="absolute bottom-4 left-0 right-0 text-center">
+       
+        <p className="text-xs text-gray-500 mt-1 bottom-0">© The Global Buzz, Inc. 2024</p>
+      </div> */}
     </main>
   );
 };
 
-export default LoginPageComp ;
+export default LoginPageComp;
--- Frontend/news/src/app/(auth)/signup/page.tsx
+++ Frontend/news/src/app/(auth)/signup/page.tsx
@@ -1,9 +1,136 @@
+// 'use client';
+// import axios from 'axios';
+// //import { Button } from '@/components/button';
+// import Head from 'next/head';
+// import { useState } from 'react';
+// import { FcGoogle } from "react-icons/fc";
+
+// export default function SignUp() {
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     email: '',
+//     phone: '',
+//     password: ''
+//   });
+
+//   const handleChange = (e: { target: { name: any; value: any; }; }) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     try {
+//       const response = await axios.post('/api/auth/register', {
+//         username: formData.name,
+//         password: formData.password,
+//         email: formData.email
+//       });
+      
+//       if (response.status === 200) {
+//         alert(response.data.message);
+//       }
+//     } catch (error) {
+//       console.error('Error during sign up:', error);
+//       alert('Failed to sign up. Please try again.');
+//     }
+//   };
+
+
+//   return (
+//     <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-4 sm:p-6 md:p-8">
+      
+
+//       <main className="w-full max-w-md">
+//         <h1 className="text-center mb-6 text-lg sm:text-xl md:text-2xl px-4 italic">
+//           Join our newsletter to receive the latest news, articles, and updates directly in your inbox!
+//         </h1>
+        
+//         <Head>
+//         <title>SIGN UP</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//         <div className="bg-gray-800 rounded-lg p-6 sm:p-8">
+//           <h2 className="text-xl sm:text-2xl font-bold mb-6 text-center">Sign Up</h2>
+//           <form onSubmit={handleSubmit} className="space-y-4">
+//             <input
+//               type="text"
+//               name="name"
+//               placeholder="Enter your Name"
+//               className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+//               onChange={handleChange}
+//               required
+//             />
+//             <input
+//               type="email"
+//               name="email"
+//               placeholder="Enter your Email"
+//               className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+//               onChange={handleChange}
+//               required
+//             />
+//             <input
+//               type="tel"
+//               name="phone"
+//               placeholder="Enter your Phone No."
+//               className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+//               onChange={handleChange}
+//               required
+//             />
+//             <input
+//               type="password"
+//               name="password"
+//               placeholder="Enter Password"
+//               className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+//               onChange={handleChange}
+//               required
+//             />
+//             <button
+//               type="submit"
+//               className="w-full bg-blue-600 hover:bg-blue-700 rounded-full py-3 text-white font-bold"
+//             >
+//               SUBMIT
+//             </button>
+             
+//           </form>
+
+//           <div className="mt-6 text-center">
+//             <p className="text-sm text-gray-300 p-1">OR</p>
+//             <p></p>
+//             </div>
+//             <div>
+//             <button className="flex items-center justify-center w-full gap-4 bg-white text-black hover:bg-gray-100 rounded-full py-2 px-4">
+//             <FcGoogle className="text-xl" />
+//             <span>Sign Up with Google</span>
+//             </button>
+//           </div>
+
+//           <p className="mt-6 text-center text-sm text-gray-400">
+//             Already have an account? <a href="/login" className="text-blue-400 hover:underline">Login</a>
+//           </p>
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 'use client';
 import axios from 'axios';
-//import { Button } from '@/components/button';
-import Head from 'next/head';
 import { useState } from 'react';
 import { FcGoogle } from "react-icons/fc";
+import { FaFacebookF } from 'react-icons/fa';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({
@@ -35,79 +162,91 @@ export default function SignUp() {
     }
   };
 
-
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-4 sm:p-6 md:p-8">
-      <Head>
-        <title>SIGN UP</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <main className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
+      <div className="sm:w-full md:w-4/5 max-w-6xl bg-white rounded-lg shadow-md overflow-hidden flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2 p-8 flex flex-col justify-center">
+          <h1 className="text-4xl font-bold text-black text-center mb-4">The Global Buzz</h1>
+          <p className="text-gray-600 text-center text-sm mb-8">"Delivering the World's Hottest Buzz, Packed with Global Insight"</p>
 
-      <main className="w-full max-w-md">
-        <h1 className="text-center mb-6 text-lg sm:text-xl md:text-2xl px-4 italic">
-          Join our newsletter to receive the latest news, articles, and updates directly in your inbox!
-        </h1>
+          <p className="text-xs text-gray-500 mb-4">By continuing you indicate that you agree to The Global Buzz's Terms of Service and Privacy Policy.</p>
 
-        <div className="bg-gray-800 rounded-lg p-6 sm:p-8">
-          <h2 className="text-xl sm:text-2xl font-bold mb-6 text-center">Sign Up</h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
+          <button className="w-full flex items-center justify-center gap-2 bg-white border border-gray-300 text-gray-700 rounded-md py-2 px-4 hover:bg-gray-50 mb-4">
+            <FcGoogle className="text-xl" />
+            <span>Continue with Google</span>
+          </button>
+
+          <button className="w-full flex items-center justify-center gap-2 bg-blue-600 text-white rounded-md py-2 px-4 hover:bg-blue-700 mb-4">
+            <FaFacebookF className="text-xl" />
+            <span>Continue with Facebook</span>
+          </button>
+
+          <div className="text-center text-sm text-gray-500">
+            <a href="/main" className="hover:underline">Continue as Guest</a><br/>
+            <div>
+            <a className="text-xs">Already have an Account? </a>
+            <a href="/login" className="hover:underline text-xs">Log In</a>
+            
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2 p-3 bg-gray-50 flex flex-col justify-center">
+          <h2 className="text-2xl font-semibold text-center mb-6">Sign Up</h2>
+          <form onSubmit={handleSubmit} className="space-y-2 pt-0 p-8 ">
+          <div className="space-y-0">
+          <div className="text-sm m-0 p-0">Name:</div>
             <input
               type="text"
               name="name"
               placeholder="Enter your Name"
-              className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full p-2 border border-gray-300 rounded-md text-xs"
               onChange={handleChange}
               required
             />
+          </div>
+          <div className="space-y-0">
+          <div className="text-sm">Email:</div>
             <input
               type="email"
               name="email"
               placeholder="Enter your Email"
-              className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full p-2 border border-gray-300 rounded-md text-xs"
               onChange={handleChange}
               required
             />
+            </div>
+            <div className="space-y-0">
+            <div className="text-sm">Phone No.:</div>
             <input
               type="tel"
               name="phone"
               placeholder="Enter your Phone No."
-              className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full p-2 border border-gray-300 rounded-md text-xs"
               onChange={handleChange}
               required
             />
+            </div>
+            <div className="space-y-0">
+            <div className="text-sm">Password:</div>
             <input
               type="password"
               name="password"
               placeholder="Enter Password"
-              className="w-full p-5 bg-gray-700 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full p-2 border border-gray-300 rounded-md text-xs"
               onChange={handleChange}
               required
             />
+            </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 hover:bg-blue-700 rounded-full py-3 text-white font-bold"
+              className="w-full bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-md"
             >
               SUBMIT
             </button>
-             
           </form>
-
-          <div className="mt-6 text-center">
-            <p className="text-sm text-gray-300 p-1">OR</p>
-            <p></p>
-            </div>
-            <div>
-            <button className="flex items-center justify-center w-full gap-4 bg-white text-black hover:bg-gray-100 rounded-full py-2 px-4">
-            <FcGoogle className="text-xl" />
-            <span>Sign Up with Google</span>
-            </button>
-          </div>
-
-          <p className="mt-6 text-center text-sm text-gray-400">
-            Already have an account? <a href="/login" className="text-blue-400 hover:underline">Login</a>
-          </p>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
