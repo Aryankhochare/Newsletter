8e263cb761e348ca9bfad1260294cf0ae325c593
Kushi-09 <159606408+Kushi-09@users.noreply.github.com>
2024-08-21 17:40:35 +0530

    main page grid layout, sidebar

--- Frontend/news/src/components/LatestNewsCarousel.tsx
+++ Frontend/news/src/components/LatestNewsCarousel.tsx
@@ -1,8 +1,106 @@
+// "use client";
+
+// import React from 'react';
+// import Link from "next/link";
+// import { ChevronLeft, ChevronRight } from 'lucide-react';
+// import { useMainStore } from '@/components/ArticleStore';
+// import parse from 'html-react-parser';
+
+// interface Article {
+//   id: string;
+//   userId: string;
+//   userName: string;
+//   categoryId: string;
+//   categoryName: string;
+//   title: string;
+//   editorContent: string;
+//   postedOn: string;
+//   modifiedOn: string;
+//   isVerified: boolean;
+//   coverImage: string;
+//   isRejected: boolean;
+// }
+
+// const ArticleLink: React.FC<{ article: Article, children: React.ReactNode }> = ({ article, children }) => {
+//   const send_article = useMainStore(state => state.setMainArticle);
+
+//   const handleClick = (e:React.MouseEvent) => {
+//     const article_content = article;
+//     if (article_content) {
+//       send_article(article_content);
+//     } else {
+//       console.log("No content found !");
+//     }
+//   }
+
+//   return (
+//     <Link href={`/main/newspage/${article.title}`} onClick={handleClick}>
+//       {children}
+//     </Link>
+//   );
+// };
+
+// const LatestNewsCarousel: React.FC<{ articles: Article[] }> = ({ articles }) => {
+//   const scrollRef = React.useRef<HTMLDivElement>(null);
+
+//   const scroll = (scrollOffset: number) => {
+//     if (scrollRef.current) {
+//       scrollRef.current.scrollLeft += scrollOffset;
+//     }
+//   };
+
+//   return (
+//     <div className="relative">
+//       <div 
+//         ref={scrollRef}
+//         className="flex overflow-x-auto space-x-4 pb-4 scrollbar-hide"
+//         style={{ scrollBehavior: 'smooth' }}
+//       >
+//         {articles.map((article, index) => (
+//           <div key={index} className="flex-none w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
+//             <ArticleLink article={article}>
+//               <div className="bg-white rounded-lg overflow-hidden shadow-lg h-full flex flex-col cursor-pointer">
+//                 <div className="relative h-48">
+//                   <img
+//                     src={article.coverImage}
+//                     alt={article.title}
+//                     className="w-full h-full object-cover"
+//                   />
+//                 </div>
+//                 <div className="p-4 flex-grow">
+//                   <span className="inline-block bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mb-2">
+//                     {article.categoryName}
+//                   </span>
+//                   <h3 className="text-xl font-semibold mb-2">{parse(article.title)}</h3>
+//                   <div className="mb-2">{parse(article.editorContent.substring(0, 150))}...</div>
+//                   <p className="text-sm text-gray-500 mt-auto">Posted on: {article.postedOn}</p>
+//                 </div>
+//               </div>
+//             </ArticleLink>
+//           </div>
+//         ))}
+//       </div>
+//       <button
+//         onClick={() => scroll(-300)}
+//         className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition-all"
+//       >
+//         <ChevronLeft className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => scroll(300)}
+//         className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition-all"
+//       >
+//         <ChevronRight className="w-6 h-6" />
+//       </button>
+//     </div>
+//   );a
+// };
+
+// export default LatestNewsCarousel;
 "use client";
 
 import React from 'react';
 import Link from "next/link";
-import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { useMainStore } from '@/components/ArticleStore';
 import parse from 'html-react-parser';
 
@@ -24,7 +122,7 @@ interface Article {
 const ArticleLink: React.FC<{ article: Article, children: React.ReactNode }> = ({ article, children }) => {
   const send_article = useMainStore(state => state.setMainArticle);
 
-  const handleClick = (e:React.MouseEvent) => {
+  const handleClick = (e: React.MouseEvent) => {
     const article_content = article;
     if (article_content) {
       send_article(article_content);
@@ -41,57 +139,31 @@ const ArticleLink: React.FC<{ article: Article, children: React.ReactNode }> = (
 };
 
 const LatestNewsCarousel: React.FC<{ articles: Article[] }> = ({ articles }) => {
-  const scrollRef = React.useRef<HTMLDivElement>(null);
-
-  const scroll = (scrollOffset: number) => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollLeft += scrollOffset;
-    }
-  };
-
   return (
-    <div className="relative">
-      <div 
-        ref={scrollRef}
-        className="flex overflow-x-auto space-x-4 pb-4 scrollbar-hide"
-        style={{ scrollBehavior: 'smooth' }}
-      >
-        {articles.map((article, index) => (
-          <div key={index} className="flex-none w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
-            <ArticleLink article={article}>
-              <div className="bg-white rounded-lg overflow-hidden shadow-lg h-full flex flex-col cursor-pointer">
-                <div className="relative h-48">
-                  <img
-                    src={article.coverImage}
-                    alt={article.title}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <div className="p-4 flex-grow">
-                  <span className="inline-block bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mb-2">
-                    {article.categoryName}
-                  </span>
-                  <h3 className="text-xl font-semibold mb-2">{parse(article.title)}</h3>
-                  <div className="mb-2">{parse(article.editorContent.substring(0, 150))}...</div>
-                  <p className="text-sm text-gray-500 mt-auto">Posted on: {article.postedOn}</p>
-                </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {articles.map((article, index) => (
+        <div key={index} className="flex-none  w-full">
+          <ArticleLink article={article}>
+            <div className="bg-white rounded-lg overflow-hidden shadow-lg h-full flex flex-col cursor-pointer hover:shadow-xl hover:transform hover:scale-105 transition-transform duration-300">
+              <div className="relative h-48">
+                <img
+                  src={article.coverImage}
+                  alt={article.title}
+                  className="w-full h-full object-cover"
+                />
               </div>
-            </ArticleLink>
-          </div>
-        ))}
-      </div>
-      <button
-        onClick={() => scroll(-300)}
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition-all"
-      >
-        <ChevronLeft className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => scroll(300)}
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition-all"
-      >
-        <ChevronRight className="w-6 h-6" />
-      </button>
+              <div className="p-4 flex-grow">
+                <span className="inline-block bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mb-2">
+                  {article.categoryName}
+                </span>
+                <h3 className="text-xl font-semibold mb-2">{parse(article.title)}</h3>
+                <div className="mb-2">{parse(article.editorContent.substring(0, 150))}...</div>
+                <p className="text-sm text-gray-500 mt-auto">Posted on: {article.postedOn}</p>
+              </div>
+            </div>
+          </ArticleLink>
+        </div>
+      ))}
     </div>
   );
 };
--- Frontend/news/src/components/navbarcomp/Sidebar.tsx
+++ Frontend/news/src/components/navbarcomp/Sidebar.tsx
@@ -1,6 +1,153 @@
+// "use client"
+// import React, { useState, useEffect, useRef } from 'react';
+// import Link from 'next/link';
+
+// interface SidebarProps {
+//   isOpen: boolean;
+//   onClose: () => void;
+// }
+
+// interface Category {
+//   categoryId: string;
+//   categoryName: string;
+// }
+
+// const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
+//   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+//   const sidebarRef = useRef<HTMLDivElement>(null);
+//   const categoriesRef = useRef<HTMLDivElement>(null);
+//   const [categories, setCategories] = useState<Category[]>([]);
+
+//   useEffect(() => {
+//     const handleClickOutside = (event: MouseEvent) => {
+//       if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node) && isOpen) {
+//         onClose();
+//       }
+//     };
+
+//     document.addEventListener('mousedown', handleClickOutside);
+//     return () => {
+//       document.removeEventListener('mousedown', handleClickOutside);
+//     };
+//   }, [isOpen, onClose]);
+
+//   useEffect(() => {
+//     if (categoriesRef.current) {
+//       if (isDropdownOpen) {
+//         categoriesRef.current.style.maxHeight = `${categoriesRef.current.scrollHeight}px`;
+//       } else {
+//         categoriesRef.current.style.maxHeight = '0';
+//       }
+//     }
+//   }, [isDropdownOpen]);
+
+//   useEffect(() => {
+//     fetchCategories();
+//   }, []);
+
+//   const fetchCategories = async () => {
+//     try {
+//       const response = await fetch('https://globalbuzz.azurewebsites.net/api/category');
+//       const data = await response.json();
+//       setCategories(data);
+//     } catch (error) {
+//       console.error('Error fetching categories:', error);
+//     }
+//   };
+
+//   const handleCategoryClick = (categoryName: string) => {
+//     onClose(); // Close the sidebar after navigation
+//   };
+
+//   return (
+//     <div
+//       ref={sidebarRef}
+//       className={`fixed top-0 left-0 h-full w-72 bg-black text-gray-100 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'
+//         } transition-transform duration-300 ease-in-out z-50 overflow-y-auto scrollbar-hide`}
+//     >
+//       <div className="p-6 scrollbar-hide">
+//         <button
+//           onClick={onClose}
+//           className="absolute top-4 right-4 text-gray-300 hover:text-white transition-colors duration-200 scrollbar-hide"
+//           aria-label="Close menu"
+//         >
+//           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+//           </svg>
+//         </button>
+//         <h2 className="text-2xl font-bold mb-6 text-white static">Menu</h2>
+//         <nav>
+//           <ul className="space-y-4">
+//             <li>
+//               <Link href="/main" className="block py-2 px-4 rounded hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+//                 Home
+//               </Link>
+//             </li>
+//             <li>
+//               <button
+//                 onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+//                 className="flex border-white border-4 items-center justify-between w-full py-2 px-4 rounded hover:bg-gray-800 transition-colors duration-200 scrollbar-hide"
+//               >
+//                 <span>Categories</span>
+//                 <svg
+//                   className={`w-4 h-4 transition-transform duration-200 ${isDropdownOpen ? 'rotate-180' : ''}`}
+//                   fill="none"
+//                   stroke="currentColor"
+//                   viewBox="0 0 24 24"
+//                   xmlns="http://www.w3.org/2000/svg"
+//                 >
+//                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+//                 </svg>
+//               </button>
+//               <div 
+//                 ref={categoriesRef}
+//                 className="mt-1 transition-all duration-300 ease-in-out overflow-hidden"
+//                 style={{ maxHeight: '0' }}
+//               >
+//                 <ul className="space-y-2 border-white border-t-0 border-4 max-h-64 overflow-y-auto scrollbar-hide">
+//                   {categories.map((category) => (
+//                     <li key={category.categoryId}>
+//                       <Link
+//                         // href={`/category/${category.categoryName}`}
+//                         href = {`/main/category/${category.categoryName}`}
+//                         className="scrollbar-hide block py-2 px-3 text-sm text-gray-300 hover:text-white hover:bg-gray-800 rounded transition-colors duration-200 w-full text-left"
+//                         onClick={() => handleCategoryClick(category.categoryName)}
+//                       >
+//                         {category.categoryName}
+//                       </Link>
+//                     </li>
+//                   ))}
+//                 </ul>
+//               </div>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
+
 "use client"
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import {
+  User, 
+  LayoutDashboard, 
+  UserCog, 
+  Inbox, 
+  Settings, 
+  FileText, 
+  PenTool, 
+  Calendar, 
+  Bookmark, 
+  History, 
+  CreditCard, 
+  LogOut, 
+  SignpostBig
+} from 'lucide-react';
 
 interface SidebarProps {
   isOpen: boolean;
@@ -13,6 +160,8 @@ interface Category {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
+  const { data: session } = useSession();
+  const userRoles =  (session?.user as any)?.roles || [];
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const sidebarRef = useRef<HTMLDivElement>(null);
   const categoriesRef = useRef<HTMLDivElement>(null);
@@ -59,6 +208,89 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
     onClose(); // Close the sidebar after navigation
   };
 
+  const renderProfileMenuItems = () => {
+    if (userRoles.includes('ADMIN')) {
+      return (
+        <>
+        {/* <Link href="/main/admin/profile" onClick={onClose}>
+          <div className="flex items-center py-4 px-2 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+          </div>
+        </Link> */}
+        <Link href="/main/admin/dashboard" onClick={onClose}>
+          <div className="flex items-center py-4 px-2 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <span>Dashboard</span>
+          </div>
+        </Link>
+        <Link href="/main/admin/roles" onClick={onClose}>
+          <div className="flex items-center py-4 px-2 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+            <UserCog className="mr-2 h-4 w-4" />
+            <span>Manage Roles</span>
+          </div>
+        </Link>
+        <Link href="/main/editor" onClick={onClose}>
+            <div className="flex items-center py-4 px-2 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+              <FileText className="mr-2 h-4 w-4" />
+              <span>Editor's Articles Page</span>
+            </div>
+          </Link>
+          <Link href="/main/writer/quill_editor" onClick={onClose}>
+            <div className="flex items-center py-4 px-4 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+              <PenTool className="mr-2 h-4 w-4" />
+              <span>Writer's Workspace</span>
+            </div>
+          </Link>
+        <Link href="/main/admin/requests" onClick={onClose}>
+          <div className="flex items-center py-4 px-2 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+            <Inbox className="mr-2 h-4 w-4" />
+            <span>Requests</span>
+          </div>
+        </Link>
+        
+        </>
+      );
+    } else if (userRoles.includes('EDITOR')) {
+      return (
+        <>
+          <Link href="/main/editor/profile" onClick={onClose}>
+            <div className="flex items-center py-4 px-2 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </div>
+          </Link>
+          <Link href="/main/editor" onClick={onClose}>
+            <div className="flex items-center py-4 px-2 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+              <FileText className="mr-2 h-4 w-4" />
+              <span>Articles</span>
+            </div>
+          </Link>
+          
+        </>
+      );
+    } else if (userRoles.includes('WRITER')) {
+      return (
+        <>
+          <Link href="/main/writer/profile" onClick={onClose}>
+            <div className="flex items-center py-4 px-4 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </div>
+          </Link>
+          <Link href="/main/writer/quill_editor" onClick={onClose}>
+            <div className="flex items-center py-4 px-4 hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+              <PenTool className="mr-2 h-4 w-4" />
+              <span>Workspace</span>
+            </div>
+          </Link>
+         
+        </>
+      );
+    }
+    return null;
+  };
+
   return (
     <div
       ref={sidebarRef}
@@ -79,7 +311,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
         <nav>
           <ul className="space-y-4">
             <li>
-              <Link href="/main" className="block py-2 px-4 rounded hover:bg-gray-800 transition-colors duration-200 scrollbar-hide">
+              <Link href="/main" className="block py-2 px-4 rounded hover:bg-gray-800 transition-colors duration-200 scrollbar-hide" onClick={onClose}>
                 Home
               </Link>
             </li>
@@ -120,6 +352,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
                 </ul>
               </div>
             </li>
+            {renderProfileMenuItems()}
           </ul>
         </nav>
       </div>
